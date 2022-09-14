8c91cbec0aaab671ff188feccf6432a21c474026
YaroslavaShyt <yaroslavashyt@gmail.com>
2022-09-14 18:48:33 +0300

    Import

---
+++ LR-1_Task-1.py
@@ -0,0 +1,34 @@
+import argparse
+
+
+def calculation(x, operator, y):
+    match operator:
+        case '+':
+            print(x + y)
+        case '-':
+            print(x - y)
+        case '*':
+            print(x * y)
+        case '/':
+            try:
+                print(x / y)
+            except ZeroDivisionError:
+                print('Division by zero error!')
+        case other:
+            raise Exception('no operator found')
+
+
+def main():
+    parser = argparse.ArgumentParser(exit_on_error=False) # Initialize the parser
+    parser.add_argument('x', type=float)                  # Add parameters positional/optional
+    parser.add_argument('operator')
+    parser.add_argument('y', type=float)
+    try:
+        args = parser.parse_args()                        # Parse the arguments
+        calculation(args.x, args.operator, args.y)
+    except Exception as ex:
+        print('Something went wrong. Check values or operator:', ex)
+
+
+main()  # Run code
+
