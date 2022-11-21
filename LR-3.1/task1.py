46665af8aa8626df10a651013b5166d62b1b89c6
Yaroslava Shyt <104785582+YaroslavaShyt@users.noreply.github.com>
2022-11-21 11:41:25 +0200

    LR 3.1 v1

---
+++ LR-3.1/task1.py
@@ -0,0 +1,126 @@
+import math
+import operator
+import warnings
+
+
+class Rational:
+    def __init__(self, num=1, den=1):
+        self.num = num
+        self.den = den
+
+    @property
+    def num(self):
+        return self.__num
+
+    @property
+    def den(self):
+        return self.__den
+
+    @num.setter
+    def num(self, new_num):
+        if not isinstance(new_num, int):
+            raise TypeError('Incorrect value for numerator!')
+        self.__num = new_num
+
+    @den.setter
+    def den(self, new_den):
+        if not isinstance(new_den, int):
+            raise TypeError('Incorrect value for denominator!')
+        elif new_den == 0:
+            raise ZeroDivisionError('Denominator cannot be 0!')
+        self.__den = new_den
+
+    def __str__(self):
+        if self.__den == 1:
+            return f'{self.num}'
+        return f'{self.num//math.gcd(self.num, self.den)}/{self.den//math.gcd(self.num, self.den)}'
+
+    def __float__(self):
+        return self.num / self.den
+
+    def __add__(self, frac):
+        if isinstance(frac, float):
+            return float(self) + frac
+        elif isinstance(frac, int):
+            frac = Rational(frac)
+        elif not isinstance(frac, Rational):
+            return NotImplemented
+        return Rational(self.num * frac.den + self.den * frac.num, self.den * frac.den)
+
+    def __sub__(self, frac):
+        return self + -frac
+
+    def __mul__(self, frac):
+        if isinstance(frac, float):
+            return float(self) * frac
+        elif isinstance(frac, int):
+            frac = Rational(frac)
+        elif not isinstance(frac, Rational):
+            return NotImplemented
+        return Rational(self.num * frac.num, self.den * frac.den)
+
+    def __truediv__(self, frac):
+        if isinstance(frac, float):
+            return float(self) / frac
+        elif isinstance(frac, int):
+            frac = Rational(frac)
+        elif not isinstance(frac, Rational):
+            return NotImplemented
+        return Rational(self.num * frac.den, self.den * frac.num)
+
+    def __radd__(self, frac):
+        return self + frac
+
+    def __rsub__(self, frac):
+        return -self + frac
+
+    def __rmul__(self, frac):
+        return self * frac
+
+    def __rtruediv__(self, frac):
+        return Rational(self.den, self.num) * frac
+
+    def comparison(self, frac, operation):
+        if isinstance(frac, float):
+            warnings.warn('Float comparison may have troubles '
+                          'due to computer rules!')
+            return operation(float(self), frac)
+        elif isinstance(frac, int):
+            frac = Rational(frac)
+        elif not isinstance(frac, Rational):
+            return NotImplemented
+        return operation(self.num * frac.den, self.den * frac.num)
+
+    def __lt__(self, frac):
+        return self.comparison(frac, operator.lt)
+
+    def __le__(self, frac):
+        return self.comparison(frac, operator.le)
+
+    def __gt__(self, frac):
+        return self.comparison(frac, operator.gt)
+
+    def __ge__(self, frac):
+        return self.comparison(frac, operator.ge)
+
+    def __eq__(self, frac):
+        return self.comparison(frac, operator.eq)
+
+    def __neg__(self):
+        return Rational(-self.num, self.den)
+
+
+try:
+    r1 = Rational(2, 5)
+    r2 = Rational(3, 6)
+    print(f'Fractions: {r1} {r2}\n'
+          f'Floats: {float(r1)} {float(r2)}\n'
+          f'Plus: {r1 + r2} | {r1 + 0.3}\n'
+          f'Minus: {r1 - r2}\n'
+          f'Multiply: {r1 * r2}\n'
+          f'Divide: {r1 / r2}\n'
+          f'Less|Less or equal: {r1 < r2} | {r1 <= r2}\n'
+          f'Greater|Greater or equal: {r1 > r2} | {r1 >= r2}\n'
+          f'Equal: {r1 == r2}')
+except Exception as ex:
+    print(ex)
---
+++ LR-3.1/task2.py
@@ -0,0 +1,164 @@
+import re
+
+
+class Item:
+    def __init__(self, code, name, price, quantity):
+        self.code = code
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    @property
+    def code(self):
+        return self.__code
+
+    @code.setter
+    def code(self, new_c):
+        if not isinstance(new_c, str):
+            raise TypeError('Incorrect type for item code!')
+        if not re.search('[0-9]+', new_c):
+            raise ValueError('Incorrect value for item code!')
+        self.__code = new_c
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def price(self):
+        return self.__price
+
+    @name.setter
+    def name(self, new_n):
+        if not isinstance(new_n, str):
+            raise TypeError('Incorrect type for item name!')
+        elif re.search(r'[^\'A-Za-z\s-]', new_n):
+            raise ValueError('Incorrect character in item name!')
+        self.__name = new_n
+
+    @price.setter
+    def price(self, new_p):
+        if not isinstance(new_p, int | float):
+            raise TypeError('Incorrect type for item price!')
+        if new_p < 0:
+            raise ValueError('Item price cannot be less 0')
+        self.__price = new_p
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+lst_of_goods = [Item('9999', "Soap with strawberries", 12, 100),
+                Item('1234', "Mint toothpaste", 20, 50),
+                Item('3241', "Shampoo with chocolate", 60, 150)]
+
+
+class Composition:
+
+    def __init__(self):
+        self.goods = []
+        self.index = 0
+
+    def add_item(self, i):
+        self.goods = self.goods + [i]
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.goods):
+            self.index += 1
+            return self.index, self.goods[self.index - 1]
+        raise StopIteration
+
+    def __setitem__(self, index, value):
+        self.goods[index] = value
+
+    def __getitem__(self, index):
+        return self.goods[index]
+
+    def __len__(self):
+        return len(self.goods)
+
+
+class Menu:
+
+    def show_menu(self, items):
+        while True:
+            ch = input(f'Choose menu option or exit(e):\n'
+                       f'1. Add info\n'
+                       f'2. Get info\n'
+                       f'3. Form report all-left\n')
+            match ch:
+                case '1':
+                    self.add_info(items)
+                case '2':
+                    self.get_info(items)
+                case '3':
+                    self.form_report(items)
+                case 'e':
+                    break
+                case _:
+                    print('Wrong command!')
+
+    def add_info(self, items):
+        self.get_info(items)
+        while True:
+            choice = input('Enter the number of item you want to change or exit (e): ')
+            if choice != 'e':
+                if int(choice) <= len(items):
+                    option = input('Enter option you want to change (1-name, 2-price, 3-quantity, e-exit): ')
+                    match option:
+                        case '1':
+                            name = input('Enter new name: ')
+                            items[int(choice)-1].name = name
+                        case '2':
+                            price = input('Enter new price: ')
+                            items[int(choice) - 1].price = float(price)
+                        case '3':
+                            quantity = input('Enter new quantity: ')
+                            items[int(choice) - 1].quantity = int(quantity)
+                        case 'e':
+                            break
+                        case _:
+                            print('Unknown command!')
+                else:
+                    print('This item does not exist!')
+            else:
+                break
+
+    @staticmethod
+    def get_info(items):
+        for num, i in items:
+            print(num, i)
+
+    @staticmethod
+    def search_item(items, code):
+        for num, obj in items:
+            if obj.code == code:
+                return obj
+        return 0
+
+    @staticmethod
+    def form_report(items):
+        while True:
+            choice = input('Enter code of item you want to get report on or exit(e): ')
+            if choice == 'e':
+                break
+            elif int(choice) <= len(items):
+                print(f'Title: {items[int(choice) - 1]} \nPrice: {items[int(choice) - 1].price}'
+                      f'\nAll-left: {items[int(choice) - 1].quantity}')
+            else:
+                print('No such item.')
+
+
+try:
+    ItemsBase = Composition()
+    for i in lst_of_goods:
+        ItemsBase.add_item(i)
+    m = Menu()
+    m.show_menu(ItemsBase)
+except Exception as ex:
+    print(ex)
+
